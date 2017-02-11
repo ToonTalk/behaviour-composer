65500712c86da59433ca43d8c1bfee0e03560585
Ken Kahn <toontalk@gmail.com>
2017-02-11 10:53:12 +0800

    EGM slider count no longer generates warning
    There no longer is limit of 8 for good placement.

--- BC/src/uk/ac/lkl/client/SliderCheckBox.java
+++ BC/src/uk/ac/lkl/client/SliderCheckBox.java
@@ -1,34 +1,29 @@
 package uk.ac.lkl.client;
 
-import uk.ac.lkl.shared.CommonUtils;
-
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 
 public class SliderCheckBox extends SessionEventsCheckBox {
-    
+
     public SliderCheckBox(
-	    String nameId,
-	    String label,
-	    String guid, 
-	    String doMessage,
-	    String undoMessage,
-	    String title,
-	    BehaviourComposer behaviourComposer) {
-	super(nameId, label, guid, doMessage, undoMessage, title, behaviourComposer);
+            String nameId,
+            String label,
+            String guid, 
+            String doMessage,
+            String undoMessage,
+            String title,
+            BehaviourComposer behaviourComposer) {
+        super(nameId, label, guid, doMessage, undoMessage, title, behaviourComposer);
     }
-    
+
     @Override
     public void onValueChange(ValueChangeEvent<Boolean> event) {
-	super.onValueChange(event);
-	int numberOfSlidersInEpidemicGame = behaviourComposer.getNumberOfSlidersInEpidemicGame();
-	if (getValue()) {
-	    behaviourComposer.setNumberOfSlidersInEpidemicGame(numberOfSlidersInEpidemicGame+1);
-	    if (numberOfSlidersInEpidemicGame >= 8) {
-		extraDoMessage = CommonUtils.stronglyHighlight(Modeller.constants.onlyRoomFor8Sliders());
-	    }
-	} else {
-	    behaviourComposer.setNumberOfSlidersInEpidemicGame(numberOfSlidersInEpidemicGame-1);
-	}
+        super.onValueChange(event);
+        int numberOfSlidersInEpidemicGame = behaviourComposer.getNumberOfSlidersInEpidemicGame();
+        if (getValue()) {
+            behaviourComposer.setNumberOfSlidersInEpidemicGame(numberOfSlidersInEpidemicGame+1);
+        } else {
+            behaviourComposer.setNumberOfSlidersInEpidemicGame(numberOfSlidersInEpidemicGame-1);
+        }
     }
 
 }
